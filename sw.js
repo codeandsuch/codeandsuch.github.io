4af4e31edf8ab5e3a765368fbd28c71aa6555544
Kalle Bornemark <kalle.bornemark@hiq.se>
2018-08-13 15:40:18 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/codestuff/app-b571f0d60357f8135f2c.js","b73480fbf91e5c324038ab935fce50c2"],["/codestuff/app-e9cbad3209dea4419c2f.js","acaaaec63ce554816ad272914a69c8a0"],["/codestuff/commons-c9acf4e3787ec326228c.js","b2c34aebc3feb0e41e52245eb4b98414"],["/codestuff/index.html","d677900297f45c3818b1083e4e3ce9f1"],["/codestuff/offline-plugin-app-shell-fallback/index.html","c5af8250db76a3f9e2a6f9cff93bd29f"]];
+var precacheConfig = [["/codestuff/app-b571f0d60357f8135f2c.js","b73480fbf91e5c324038ab935fce50c2"],["/codestuff/app-e9cbad3209dea4419c2f.js","a37739edec8d2feba7608486427a24ff"],["/codestuff/commons-c9acf4e3787ec326228c.js","1ff60adcb8b7914ccb21cde007ac4e3c"],["/codestuff/index.html","28050e13d17e8e3aa6392e9e2fca436e"],["/codestuff/offline-plugin-app-shell-fallback/index.html","f38736d0537bad51997386cbd7a4fbc0"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 

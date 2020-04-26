cae8ed9a197da82dce2e2b8a2ff25ae908b5edb4
Kalle Bornemark <kalle.bornemark@gmail.com>
2020-04-26 21:19:44 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-0fae4e4c1c98cc5f2dbb.js","4b4d8ca5be216d9f3b86cb203653a035"],["/app-119d06646962e1572ecf.js","bfe433008582f77dd66015f9e2d710ae"],["/app-3a5a6f10546aceea6f89.js","fa798d613515b71ca71c67e044a4a719"],["/commons-e8ba142de36a99798fa1.js","2b1954138d2c0865250158a9ae65616b"],["/index.html","78b7de646ed1f3e6ae4a02ed5c123c1b"],["/offline-plugin-app-shell-fallback/index.html","f76642bc7e77bc857609656d6e24bbff"]];
+var precacheConfig = [["/app-0fae4e4c1c98cc5f2dbb.js","4b4d8ca5be216d9f3b86cb203653a035"],["/app-119d06646962e1572ecf.js","bfe433008582f77dd66015f9e2d710ae"],["/app-3a5a6f10546aceea6f89.js","fa798d613515b71ca71c67e044a4a719"],["/commons-e8ba142de36a99798fa1.js","2b1954138d2c0865250158a9ae65616b"],["/index.html","55600a943e3e839c86b2a8f2cf4b07e2"],["/offline-plugin-app-shell-fallback/index.html","4e0361fabc41536fa1748a91033f77fd"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
